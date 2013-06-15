3319322a1655f705dffc5dde9265315ef25a8efd
Hunternif <hunternif@gmail.com>
2013-06-16 02:10:52 +0400

    Gold GUI is shown on the in-game screen too.

--- src/hunternif/mc/dota2items/gui/GuiGold.java
+++ src/hunternif/mc/dota2items/gui/GuiGold.java
@@ -5,6 +5,7 @@ import hunternif.mc.dota2items.core.EntityStats;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.gui.inventory.GuiInventory;
 
 import org.lwjgl.opengl.GL11;
@@ -23,27 +24,38 @@ public class GuiGold extends Gui {
 	public static final int TEXT_POSITION_Y = 8;
 	
 	public void render() {
-		// Only show when the player inventory is open
-		if (Minecraft.getMinecraft().currentScreen instanceof GuiInventory) {
-			EntityStats stats = Dota2Items.mechanics.getEntityStats(Minecraft.getMinecraft().thePlayer);
-			if (stats != null) {
-				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-				GL11.glDisable(GL11.GL_LIGHTING);
-				Minecraft.getMinecraft().renderEngine.bindTexture("/mods/"+Dota2Items.ID+"/textures/gui/gold.png");
-				int x = (Minecraft.getMinecraft().currentScreen.width - GUI_INVENTORY_WIDTH)/2 + GUI_INVENTORY_WIDTH - GUI_GOLD_WIDTH;
-				// If there are active potion effects, the inventory is shifted to the right.
-				if (!Minecraft.getMinecraft().thePlayer.getActivePotionEffects().isEmpty()) {
-					x += 60;
-				}
-				int y = (Minecraft.getMinecraft().currentScreen.height - GUI_INVENTORY_HEIGHT)/2 - GUI_GOLD_HEIGHT;
-				drawTexturedModalRect(x, y, 0, 0, GUI_GOLD_WIDTH, GUI_GOLD_HEIGHT);
-				// Draw the number
-				FontRenderer fontRenderer = Minecraft.getMinecraft().fontRenderer;
-				String text = String.valueOf(stats.getGold());
-				x += TEXT_POSITION_X - fontRenderer.getStringWidth(text);
-				y += TEXT_POSITION_Y;
-				fontRenderer.drawString(text, x, y, GOLD_COLOUR);
+		Minecraft mc = Minecraft.getMinecraft();
+		int x;
+		int y;
+		// Show gold when the inventory is open:
+		if (mc.currentScreen instanceof GuiInventory) {
+			x = (mc.currentScreen.width - GUI_INVENTORY_WIDTH)/2 + GUI_INVENTORY_WIDTH - GUI_GOLD_WIDTH;
+			// If there are active potion effects, the inventory is shifted to the right.
+			if (!mc.thePlayer.getActivePotionEffects().isEmpty()) {
+				x += 60;
 			}
+			y = (mc.currentScreen.height - GUI_INVENTORY_HEIGHT)/2 - GUI_GOLD_HEIGHT;
+		// Or in game:
+		} else if (mc.currentScreen == null) {
+			//TODO make this GUI placement configurable in the menu in order to not overlay other mods' GUIs.
+			ScaledResolution scaledresolution = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
+			x = scaledresolution.getScaledWidth() - GUI_GOLD_WIDTH;
+			y = 0;
+		} else {
+			return;
+		}
+		EntityStats stats = Dota2Items.mechanics.getEntityStats(mc.thePlayer);
+		if (stats != null) {
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			mc.renderEngine.bindTexture("/mods/"+Dota2Items.ID+"/textures/gui/gold.png");
+			drawTexturedModalRect(x, y, 0, 0, GUI_GOLD_WIDTH, GUI_GOLD_HEIGHT);
+			// Draw the number
+			FontRenderer fontRenderer = mc.fontRenderer;
+			String text = String.valueOf(stats.getGold());
+			x += TEXT_POSITION_X - fontRenderer.getStringWidth(text);
+			y += TEXT_POSITION_Y;
+			fontRenderer.drawString(text, x, y, GOLD_COLOUR);
 		}
 	}
 }

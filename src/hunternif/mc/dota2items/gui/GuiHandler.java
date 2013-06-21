9965fd17ec6498b2dd303632bcee916f8892543a
Hunternif <hunternif@gmail.com>
2013-06-21 05:24:20 +0400

    Deleted leftover old GuiShop references.

--- src/hunternif/mc/dota2items/gui/GuiHandler.java
+++ src/hunternif/mc/dota2items/gui/GuiHandler.java
@@ -1,6 +1,5 @@
 package hunternif.mc.dota2items.gui;
 
-import hunternif.mc.dota2items.inventory.ContainerShop;
 import hunternif.mc.dota2items.inventory.ContainerShopBuy;
 import hunternif.mc.dota2items.inventory.ContainerShopSell;
 import net.minecraft.entity.player.EntityPlayer;
@@ -8,15 +7,12 @@ import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler {
-	public static final int GUI_ID_SHOP = 0;
-	public static final int GUI_ID_SHOP_BUY = 1;
-	public static final int GUI_ID_SHOP_SELL = 2;
+	public static final int GUI_ID_SHOP_BUY = 0;
+	public static final int GUI_ID_SHOP_SELL = 1;
 
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		switch (ID) {
-		case GUI_ID_SHOP:
-			return new ContainerShop(player.inventory);
 		case GUI_ID_SHOP_BUY:
 			return new ContainerShopBuy();
 		case GUI_ID_SHOP_SELL:
@@ -28,8 +24,6 @@ public class GuiHandler implements IGuiHandler {
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		switch (ID) {
-		case GUI_ID_SHOP:
-			return new GuiShop(player.inventory);
 		case GUI_ID_SHOP_BUY:
 			return new GuiShopBuy(player.inventory);
 		case GUI_ID_SHOP_SELL:

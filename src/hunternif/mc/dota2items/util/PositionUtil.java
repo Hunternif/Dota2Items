3949efdf23d612a144787fdf152baa654c4d786c
Hunternif <hunternif@gmail.com>
2013-09-19 01:14:44 +0400

    Edited comments.

--- src/hunternif/mc/dota2items/util/PositionUtil.java
+++ src/hunternif/mc/dota2items/util/PositionUtil.java
@@ -10,39 +10,31 @@ import net.minecraft.world.World;
 
 public class PositionUtil {
 	/**
-	 * 
-	 * @param world
-	 * @param x startX
-	 * @param y startY
-	 * @param z startZ
-	 * @param tx endX
-	 * @param ty endY
-	 * @param tz endZ
 	 * @param borderSize 	extra area to examine around line for entities
-	 * @param excluded		any excluded entities (the player, etc)
+	 * @param excluded		any excluded entity
 	 * @return a MovingObjectPosition of either the block hit (no entity hit),
-	 *         the entity hit (hit an entity), or null for nothing hit
+	 *         the entity hit (hit an entity), or null for nothing hit.
 	 */
-	//FIXME rewrite this, because it doesn't work the way i want
-	public static MovingObjectPosition tracePath(World world, double x, double y,
-			double z, double tx, double ty, double tz, double borderSize,
-			Entity excluded, boolean checkWater) {
-		Vec3 startVec = world.getWorldVec3Pool().getVecFromPool(x, y, z);
-		Vec3 lookVec = world.getWorldVec3Pool().getVecFromPool(tx - x, ty - y, tz - z);
-		Vec3 endVec = world.getWorldVec3Pool().getVecFromPool(tx, ty, tz);
-		double minX = x < tx ? x : tx;
-		double minY = y < ty ? y : ty;
-		double minZ = z < tz ? z : tz;
-		double maxX = x > tx ? x : tx;
-		double maxY = y > ty ? y : ty;
-		double maxZ = z > tz ? z : tz;
+	public static MovingObjectPosition tracePath(World world,
+			double startX, double startY, double startZ,
+			double endX, double endY, double endZ,
+			double borderSize, Entity excluded, boolean checkWater) {
+		Vec3 startVec = world.getWorldVec3Pool().getVecFromPool(startX, startY, startZ);
+		Vec3 lookVec = world.getWorldVec3Pool().getVecFromPool(endX - startX, endY - startY, endZ - startZ);
+		Vec3 endVec = world.getWorldVec3Pool().getVecFromPool(endX, endY, endZ);
+		double minX = startX < endX ? startX : endX;
+		double minY = startY < endY ? startY : endY;
+		double minZ = startZ < endZ ? startZ : endZ;
+		double maxX = startX > endX ? startX : endX;
+		double maxY = startY > endY ? startY : endY;
+		double maxZ = startZ > endZ ? startZ : endZ;
 		AxisAlignedBB bb = AxisAlignedBB.getAABBPool()
 				.getAABB(minX, minY, minZ, maxX, maxY, maxZ)
 				.expand(borderSize, borderSize, borderSize);
 		List<Entity> allEntities = world.getEntitiesWithinAABBExcludingEntity(excluded, bb);
 		MovingObjectPosition blockHit = world.clip(startVec, endVec, checkWater);
-		startVec = world.getWorldVec3Pool().getVecFromPool(x, y, z);
-		endVec = world.getWorldVec3Pool().getVecFromPool(tx, ty, tz);
+		startVec = world.getWorldVec3Pool().getVecFromPool(startX, startY, startZ);
+		endVec = world.getWorldVec3Pool().getVecFromPool(endX, endY, endZ);
 		double maxDistance = endVec.distanceTo(startVec);
 		if (blockHit != null) {
 			maxDistance = blockHit.hitVec.distanceTo(startVec);
